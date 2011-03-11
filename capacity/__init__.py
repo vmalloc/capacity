f8c4277f78484a7a82fda8a8a22491016b9d1966
Rotem Yaari <vmalloc@gmail.com>
2011-03-12 00:08:55 +0200

    Better implementation as a numeric type

--- capacity/__init__.py
+++ capacity/__init__.py
@@ -27,35 +27,51 @@ class Capacity(object):
     def __ge__(self, other):
         return self._compare(other) >= 0
     ## Arithmetic
+    def __neg__(self):
+        return Capacity(-self.bits)
     def __mul__(self, other):
-        if not isinstance(other, Number):
-            raise TypeError("Cannot multiply capacity by %r" % (other,))
-        return self._arithmetic_to_capacity(operator.mul, other)
+        if isinstance(other, Capacity):
+            raise TypeError("Cannot multiply Capacity by Capacity")
+        return self._arithmetic_to_capacity(operator.mul, other,
+                                            allow_nonzero_ints=True)
     __rmul__ = __mul__
     def __add__(self, other):
-        if not isinstance(other, Capacity):
-            raise TypeError("Cannot add Capacity and %r objects" % (other,))
         return self._arithmetic_to_capacity(operator.add, other)
-    def __iadd__(self, other):
-        self.bits = (self + other).bits
+    __radd__ = __add__
     def __sub__(self, other):
-        if not isinstance(other, Capacity):
-            raise TypeError("Cannot subtract %r objects from Capacity" % (other,))
         return self._arithmetic_to_capacity(operator.sub, other)
-    def __isub__(self, other):
-        self.bits = (self - other).bits
+    def __rsub__(self, other):
+        return (-self) + other
     def __div__(self, other):
         if isinstance(other, Capacity):
             return self._arithmetic_to_number(operator.div, other)
-        return self._arithmetic_to_capacity(operator.div, other)
+        return self._arithmetic_to_capacity(operator.div, other, allow_nonzero_ints=True)
+    def __rdiv__(self, other):
+        if other == 0:
+            return 0
+        raise TypeError("Attempt to divide %r object by Capacity" % (other,))
+    __rfloordiv__ = __rdiv__
+    def __floordiv__(self, other):
+        return self._arithmetic_to_number(operator.floordiv, other, allow_nonzero_ints=True)
     def __mod__(self, other):
-        if isinstance(other, Capacity):
-            return self._arithmetic_to_capacity(operator.mod, other)
-        return self._arithmetic_to_number(operator.mod, other)
-    def _arithmetic_to_number(self, operator, operand):
-        return operator(self.bits, operand.bits if isinstance(operand, Capacity) else operand)
-    def _arithmetic_to_capacity(self, operator, operand):
-        return Capacity(self._arithmetic_to_number(operator, operand))
+        return self._arithmetic_to_capacity(operator.mod, other)
+    def __rmod__(self, other):
+        if other == 0:
+            return self
+        raise TypeError("Attempt to perform modulo of %r object by Capacity" % (other,))
+    def roundup(self, boundary):
+        returned = (self // boundary) * boundary
+        if self % boundary != 0:
+            returned += boundary
+        return returned
+    def _arithmetic_to_number(self, operator, operand, allow_nonzero_ints=False):
+        if not isinstance(operand, Capacity):
+            if not allow_nonzero_ints and operand != 0:
+                raise TypeError("Attempt to perform %s operation between capacity and %r object" % (operator.__name__, operand))
+            operand = Capacity(operand)
+        return operator(self.bits, operand.bits)
+    def _arithmetic_to_capacity(self, *args, **kwargs):
+        return Capacity(self._arithmetic_to_number(*args, **kwargs))
     ## Representation
     def __str__(self):
         return '{0} bit'.format(self.bits)
--- tests/test__capacity.py
+++ tests/test__capacity.py
@@ -16,11 +16,17 @@ class CapacityTest(TestCase):
         self.assertTrue(small != great)
         self.assertTrue(great != small)
         self.assertGreater(great, small)
+        self.assertGreater(small, -small)
+        self.assertGreater(great, -great)
+        self.assertGreater(0, -small)
+        self.assertLess(0, small)
+        self.assertLess(-small, 0)
         self.assertGreaterEqual(great, small)
         self.assertLess(small, great)
         self.assertLessEqual(small, great)
         #negative tests
         self.assertFalse(small > great)
+        self.assertFalse(small < -small)
         self.assertFalse(small >= great)
         self.assertFalse(great < small)
         self.assertFalse(great <= small)
@@ -28,6 +34,17 @@ class CapacityTest(TestCase):
         self.assertFalse(great != great)
         self.assertFalse(great == small)
         self.assertFalse(small == great)
+    def test__equality_to_other_objects(self):
+        for obj in ("some_string", "", 2.0, 2, True, False):
+            self.assertTrue(MiB != obj)
+            self.assertFalse(MiB == obj)
+    def test__multiplication_by_zero(self):
+        self.assertIsInstance(0 * MiB, Capacity)
+    def test__equality_to_zero(self):
+        self.assertFalse(MiB == 0)
+        self.assertTrue(MiB != 0)
+        self.assertFalse(0 * MiB != 0)
+        self.assertTrue(0 * MiB == 0)
 
 
 class RepresentationTest(TestCase):
@@ -40,13 +57,43 @@ class RepresentationTest(TestCase):
         self.assertEquals(repr(obj), value)
 
 class CapacityArithmeticTest(TestCase):
+    def test__add(self):
+        self.assertEquals(MiB + MiB, Capacity(MiB.bits * 2))
+        self.assertEquals(MiB + 0, MiB)
+        self.assertEquals(0 + MiB, MiB)
+    def test__iadd(self):
+        a = MiB
+        a += MiB
+        self.assertEquals(a, 2 * MiB)
+    def test__sub(self):
+        self.assertEquals(MiB - bit, Capacity(MiB.bits - 1))
+        self.assertEquals(0 - bit, -bit)
+        self.assertEquals(bit - 0, bit)
+    def test__isub(self):
+        a = 2 * MiB
+        a -= MiB
+        self.assertEquals(a, MiB)
+    def test__neg(self):
+        self.assertEquals((-MiB).bits, -(MiB.bits))
+        self.assertEquals(MiB + (-MiB), 0)
+        self.assertEquals(-MiB + (2 * MiB), MiB)
     def test__mul(self):
         self.assertEquals(2 * MiB, Capacity(MiB.bits * 2))
+        self.assertEquals(0 * MiB, Capacity(0))
     def test__div(self):
         self.assertEquals((2 * MiB) / 2, MiB)
+        self.assertEquals((1.5 * MiB) / MiB, 1.5)
+        self.assertEquals((2 * MiB) / MiB, 2)
+        self.assertEquals(0/MiB, 0)
     def test__mod(self):
         self.assertEquals(((2 * MiB) + bit) % MiB, bit)
-
+        self.assertEquals(0 % MiB, MiB)
+    def test__floordiv(self):
+        self.assertEquals(((2 * MiB)+bit) // MiB, 2)
+        self.assertEquals(0 // MiB, 0)
+    def test__roundup(self):
+        self.assertEquals(MiB.roundup(MiB), MiB)
+        self.assertEquals((MiB + bit).roundup(MiB), 2 * MiB)
 class InvalidArithmeticTest(TestCase):
     def test__invalid_arithmetic(self):
         size = 668 * bit
@@ -57,3 +104,13 @@ class InvalidArithmeticTest(TestCase):
             size + 3
         with self.assertRaises(TypeError):
             3 / size
+        with self.assertRaises(TypeError):
+            3 % size
+        with self.assertRaises(TypeError):
+            size % 3
+        with self.assertRaises(TypeError):
+            size < 2
+        with self.assertRaises(TypeError):
+            size > 2
+        with self.assertRaises(ZeroDivisionError):
+            size / 0
